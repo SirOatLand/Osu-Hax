e409521c5c987d609738470a1a71dc86cd215bb6
Oat <oatt16@gmail.com>
2025-11-25 11:46:54 +0700

    update to handle save_image

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from time import time
+import time
 import cv2
 import numpy as np
 from windows_capture import WindowsCapture, Frame, InternalCaptureControl
 from imgdiff import detect_imgdiff
+from save_image import save_image
 
 latest_frame = None
 
@@ -27,21 +28,33 @@ def on_closed():
     print("Capture Session Closed")
     cv2.destroyAllWindows()
 
+
+save_image_mode = True
+
 if __name__ == "__main__":
     capture.start_free_threaded()
     screenshot = None
     tempscreenshot = None
+
+    if save_image_mode:
+        for i in range(5, 0, -1):
+            print(i)
+            time.sleep(1)
     while True:
-        loop_start = time()
+        loop_start = time.time()
         if latest_frame is not None:
             if screenshot is not None:
                 tempscreenshot = screenshot
             screenshot = frame_to_numpy(latest_frame)
-            if tempscreenshot is not None:
+
+            if save_image_mode:
+                save_image(screenshot, folder_names=['img1', 'img2', 'img3', 'img4'], img_count=250, delay=0.2)
+                cv2.imshow("screenshoting", screenshot)
+            elif tempscreenshot is not None:
                 cv2.imshow("Fake Osu", detect_imgdiff(tempscreenshot, screenshot, 30))
 
         try:
-            print("FPS:", 1 / (time() - loop_start))
+            print("FPS:", 1 / (time.time() - loop_start))
         except ZeroDivisionError:
             pass
 
