b08ab5346cd2b94822a6c7f255060fe115d98839
Oat <oatt16@gmail.com>
2025-11-27 20:49:24 +0700

    add config file for second monitor case

---
+++ config.py
@@ -0,0 +1,17 @@
+SECOND_MONITOR=1920
+
+if __name__ == "__main__":
+    import pyautogui
+    pyautogui.PAUSE = 0.04
+    pyautogui.moveTo(1800 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1700 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1600 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1500 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1400 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1300 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1200 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1100 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(1000 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.moveTo(900 + SECOND_MONITOR, 500, duration=0)
+    pyautogui.mouseDown()
+    pyautogui.mouseUp()
