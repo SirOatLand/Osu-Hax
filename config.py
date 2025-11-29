b0e52bca950f3b7033775e10484a3eadd8e1308d
Oat <oatt16@gmail.com>
2025-11-29 23:53:26 +0700

    add global mouse hexcode

--- config.py
+++ config.py
@@ -1,17 +1,10 @@
 SECOND_MONITOR=1920
+MOUSE_LEFTDOWN = 0x0002     # left button down
+MOUSE_LEFTUP = 0x0004       # left button up
+MOUSE_RIGHTDOWN = 0x0008    # right button down
+MOUSE_RIGHTUP = 0x0010      # right button up
+MOUSE_MIDDLEDOWN = 0x0020   # middle button down
+MOUSE_MIDDLEUP = 0x0040     # middle button up
 
 if __name__ == "__main__":
-    import pyautogui
-    pyautogui.PAUSE = 0.04
-    pyautogui.moveTo(1800 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1700 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1600 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1500 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1400 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1300 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1200 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1100 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(1000 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.moveTo(900 + SECOND_MONITOR, 500, duration=0)
-    pyautogui.mouseDown()
-    pyautogui.mouseUp()
+    pass
