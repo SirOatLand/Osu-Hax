424c27cb49d4e630d84cea85de3668c917154177
Oat <oatt16@gmail.com>
2025-12-01 20:01:40 +0700

    add map replication and .env

--- main_vision.py
+++ main_vision.py
@@ -1,21 +1,25 @@
 import time
-
-
 import cv2
 import numpy as np
 import pyautogui
 import math
-
 from windows_capture import WindowsCapture, Frame, InternalCaptureControl
+import supervision as svi
+from inference import get_model
+from collections import deque
+from dotenv import load_dotenv
+import os
+
 from imgdiff import detect_imgdiff
 from save_image import save_image
 from osu_input import *
 from read_map import *
 from config import SECOND_MONITOR
 from coord_queue import CoordQueue, infer_to_queue
-from inference import get_model
-import supervision as svi
-from collections import deque
+from replicate_songs import add_song_queue, queue_to_file
+
+
+load_dotenv()
 
 latest_frame = None
 current_action = None
@@ -46,7 +50,7 @@ def on_closed():
     cv2.destroyAllWindows()
 
 
-def main(save_image_mode, song_path):
+def main(save_image_mode, replicate, song_path):
     global start_time
     global osu_index
     global current_action
@@ -56,27 +60,41 @@ def main(save_image_mode, song_path):
     capture.start_free_threaded()
     screenshot = None
 
+    # Map Replication
+    if replicate:
+        removed_queue = None
+        queue_record = []
+
+    # Prepare variables
     osu_objects, timing_points, slider_multiplier, time_delay_300, AR_delay = prep_osu_objects(song_path)
     osu_index = 0
     current_action = None
-    AR_delay = AR_delay - AR_DELAY_OFFSET
+    AR_delay = (AR_delay - AR_DELAY_OFFSET)
 
+    # Click Start map on osu! screen
     osu_start_x, osu_start_y = osu_to_screen(320, 170)
     pyautogui.moveTo(osu_start_x, osu_start_y)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
 
+    # Loading Model
     model = get_model(
-        model_id="osu-project-2-9xzrs/2",
-        api_key="n9ZqQYFxrPZCCverE0Lh"
+        model_id=os.getenv('MODEL_ID'),
+        api_key=os.getenv('API_KEY'),
     )
+
+    # Initializing objects queue
     coord_queue = CoordQueue(threshold_dist=OBJ_THRESHOLD, cooldown_time=OBJ_COOLDOWN, min_detect_count=OBJ_MIN_COUNT, threshold_t=0)
 
+    # Check process name change which indicate map is loaded
     wait_for_title_change(timeout=10)
+
+    first_time = time.perf_counter()
     while True:
         if latest_frame is not None:  # Keeping inferring before the game starts
             screenshot = frame_to_numpy(latest_frame)
-            infer_to_queue(model.infer(screenshot), coord_queue, screenshot, time.perf_counter()*1000)
+            time_stamp = (time.perf_counter() - first_time) * 1000
+            infer_to_queue(model.infer(screenshot), coord_queue, screenshot, time_stamp)
         left_click = ctypes.windll.user32.GetAsyncKeyState(0x01) & 0x8000
         shift_pressed = ctypes.windll.user32.GetAsyncKeyState(0x10) & 0x8000
         if left_click & shift_pressed:
@@ -89,51 +107,63 @@ def main(save_image_mode, song_path):
     initial_timestamp = time.perf_counter()
 
     while osu_index < len(osu_objects):
-        # now_t = time.perf_counter() + start_time - initial_timestamp
         loop_start = time.time()
         # ========== FRAME PROCESSING ==========
         if latest_frame is not None:
             screenshot = frame_to_numpy(latest_frame)
             results = model.infer(screenshot)
-            infer_to_queue(results, coord_queue, screenshot, time.perf_counter()*1000)
+            time_stamp = (time.perf_counter() - initial_timestamp) * 1000
+            infer_to_queue(results, coord_queue, screenshot, time_stamp)
+
+            # load the results into the supervision Detections api
+            detections = svi.Detections.from_inference(results[0])
+
+            # create supervision annotators
+            bounding_box_annotator = svi.BoxAnnotator()
+            label_annotator = svi.LabelAnnotator()
+
+            # annotate the image with our inference results
+            annotated_image = bounding_box_annotator.annotate(scene=screenshot, detections=detections)
+            annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+            screenshot = annotated_image
+
+            cv2.imshow("Fake Osu!", screenshot)
+
             results = None
         ready_to_process = []
 
         # Iterate through the queue without removing items
         for coord in coord_queue.queue:
             # print(now_t, time_detected, AR_delay, now - time_detected)
-            if (time.perf_counter()*1000) - coord.time_ms >= AR_delay:
+            time_stamp = (time.perf_counter() - initial_timestamp) * 1000
+            if (time_stamp) - coord.time_ms >= AR_delay:
                 ready_to_process.append(coord)
 
-        if current_action is None and ready_to_process:
-            coord = ready_to_process.pop(0)
+        if (current_action is None and ready_to_process)  or isinstance(osu_objects[osu_index], Spinner):
+            if ready_to_process:
+             coord = ready_to_process.pop(0)
 
             # ========== 2. MATCH TO NEXT OSU OBJECT ==========
-            while osu_index < len(osu_objects) and current_action is None:
+            while (osu_index < len(osu_objects) and current_action is None):
                 obj = osu_objects[osu_index]
-                # print(f"Obtained object is {obj}")
+                now_t = time.perf_counter() + start_time - initial_timestamp
+
                 # Match based on expected class logic
                 if isinstance(obj, HitCircle) and coord.cls == "circle":
                     x, y = ai_to_screen(coord.x, coord.y, coord.screen_x, coord.screen_y)
                     current_action = CircleAction(obj, x, y)
-                    coord_queue.remove(coord)
+                    removed_queue = coord_queue.remove(coord)
                     break
 
-                now_t = time.perf_counter() + start_time - initial_timestamp
-                # print(f"now_t is {now_t}")
                 if isinstance(obj, Slider):
-                    # print(f"Now is {now_t}, waiting for {obj.time/1000}.")
                     if now_t >= (obj.time / 1000):
-#                         print("Condition passed")
                         current_action = SliderAction(obj)
-                        coord_queue.remove(coord)
+                        removed_queue = coord_queue.remove(coord)
                         break
-#                     print("Condition not passed")
 
                 elif isinstance(obj, Spinner):
                     if now_t >= (obj.time / 1000):
                         current_action = SpinnerAction(obj)
-                        coord_queue.remove(coord)
                         break
 
                 else:
@@ -150,10 +180,7 @@ def main(save_image_mode, song_path):
                     else:
                         coord_queue.remove(coord)
                         break
-            # print(f"End of class {dcls}")
-        # print(f"Process length: {len(ready_to_process)}")
-        # for item in ready_to_process:
-        #     coord_queue.remove(*item)
+
         # ========== 3. CREATE ACTION FOR MATCHED OBJECT ==========
         # print(f"Current action is {current_action}")
         # ========== 4. UPDATE ONGOING ACTION ==========
@@ -162,7 +189,6 @@ def main(save_image_mode, song_path):
             now_t = time.perf_counter() + start_time - initial_timestamp
             current_action.update(now_t)
             if current_action.done:
-                # print("Action done")
                 current_action = None
                 osu_index += 1
 
@@ -180,7 +206,16 @@ def main(save_image_mode, song_path):
             cv2.destroyAllWindows()
             break
 
+        # ============= Song Replicate =============
+        if replicate:
+            if removed_queue is not None:
+                queue_record.append(removed_queue)
+                removed_queue = None
+
+    if replicate:
+        queue_to_file(queue_record)
 
 if __name__ == "__main__":
     # pyautogui.PAUSE = 0.05
-    main(save_image_mode=False, song_path="./test_songs/cin_normal.osu")
+    song_name = "cin_normal.osu"
+    main(save_image_mode=False, replicate=True, song_path="./test_songs/" + song_name)
